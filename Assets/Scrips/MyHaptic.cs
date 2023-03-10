2f53cd682f0ea4d1e63e27a785e73e911679ddc7
jacobyi0730 <jacobyi0730@gmail.com>
2023-03-10 14:58:16 +0900

    20230310

--- Assets/Scrips/MyHaptic.cs
+++ Assets/Scrips/MyHaptic.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class MyHaptic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MyHaptic instance;
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator CreateVibrateTime(int iteration, int frequency, int strength, OVRInput.Controller controller, float time)
     {
-        
+        var channel = controller == OVRInput.Controller.LTouch ? OVRHaptics.LeftChannel : OVRHaptics.RightChannel;
+
+        byte[] sample = new byte[iteration];
+
+        for (int i = 0; i < iteration; i++)
+        {
+            sample[i] = i % frequency == 0 ? (byte)0 : (byte)strength;
+        }
+
+        OVRHapticsClip createdClip = new OVRHapticsClip(sample, sample.Length);
+
+        for (float t = 0; t <= time; t += Time.deltaTime)
+        {
+            Debug.Log("Play vib");
+            channel.Queue(createdClip);
+        }
+        yield return new WaitForSeconds(time);
+        channel.Clear();
+        yield return null;
+    }
+
+    public void PlayVibration(bool rightHanded, int force, float time)
+    {
+        if (rightHanded) StartCoroutine(CreateVibrateTime(64, 100, force, OVRInput.Controller.RTouch, time));
+        else StartCoroutine(CreateVibrateTime(64, 100, force, OVRInput.Controller.LTouch, time));
     }
 }
--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -22,41 +22,14 @@ public class Player : MonoBehaviour
     {
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
         {
-            PlayShoot(false);
+            MyHaptic.instance.PlayVibration(false, 1000, 0.3f);
         }
 
         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
         {
-            PlayShoot(true);
+            MyHaptic.instance.PlayVibration(true, 1000, 0.3f);
         }
     }
 
-    public IEnumerator CreateVibrateTime(int iteration, int frequency, int strength, OVRInput.Controller controller, float time)
-    {
-        var channel = controller == OVRInput.Controller.LTouch ? OVRHaptics.LeftChannel : OVRHaptics.RightChannel;
-
-        byte[] sample = new byte[iteration];
-
-        for (int i = 0; i < iteration; i++)
-        {
-            sample[i] = i % frequency == 0 ? (byte)0 : (byte)strength;
-        }
-
-        OVRHapticsClip  createdClip = new OVRHapticsClip(sample, sample.Length);
-
-        for (float t = 0; t <= time; t += Time.deltaTime)
-        {
-            Debug.Log("Play vib");
-            channel.Queue(createdClip);
-        }
-        yield return new WaitForSeconds(time);
-        channel.Clear();
-        yield return null;
-    }
-
-    public void PlayShoot(bool rightHanded)
-    {
-        if (rightHanded) StartCoroutine(CreateVibrateTime(64, 100, 1000, OVRInput.Controller.RTouch, 1));
-        else StartCoroutine(CreateVibrateTime(64, 100, 1000, OVRInput.Controller.LTouch, 1));
-    }
+    
 }
