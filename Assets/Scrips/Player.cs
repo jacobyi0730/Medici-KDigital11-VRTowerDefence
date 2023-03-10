b7fe5e3f0692d7b29e23931a8f408863b592afc5
jacobyi0730 <jacobyi0730@gmail.com>
2023-03-10 13:16:23 +0900

    20230310_1316
    진동

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -14,12 +14,50 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+        {
+            PlayShoot(false);
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        {
+            PlayShoot(true);
+        }
+    }
+
+    public IEnumerator CreateVibrateTime(int iteration, int frequency, int strength, OVRInput.Controller controller, float time)
+    {
+        var channel = controller == OVRInput.Controller.LTouch ? OVRHaptics.LeftChannel : OVRHaptics.RightChannel;
+
+        OVRHapticsClip createdClip;
+        createdClip = new OVRHapticsClip(iteration);
+
+        for (int i = 0; i < iteration; i++)
+        {
+            createdClip.Samples[i] = i % frequency == 0 ? (byte)0 : (byte)strength;
+        }
+
+        createdClip = new OVRHapticsClip(createdClip.Samples, createdClip.Samples.Length);
+
+        for (float t = 0; t <= time; t += Time.deltaTime)
+        {
+            Debug.Log("Play vib");
+            channel.Queue(createdClip);
+        }
+        yield return new WaitForSeconds(time);
+        channel.Clear();
+        yield return null;
+    }
+
+    public void PlayShoot(bool rightHanded)
+    {
+        if (rightHanded) StartCoroutine(CreateVibrateTime(64, 100, 1000, OVRInput.Controller.RTouch, 1));
+        else StartCoroutine(CreateVibrateTime(64, 100, 1000, OVRInput.Controller.LTouch, 1));
     }
 }
